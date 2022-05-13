26995ca4eca99ac7fea6bf46d4d55d5180a49454
TheLifeOfSeo <dsa3182@gmail.com>
2022-05-13 23:52:10 +0900

    html 페이지 생성

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 #2. SQL 기반 DB에 저장 (JSON 사용가능하면 JSON 사용하기)
 #3. 파이선에서 필요한 데이터 불러와 출력
 #4. 웹 기반으로 사용자에게 보여주기 (그래프, 도표 등)
+
+from selenium import webdriver
+#드라이버 초기화
+driver = webdriver.Chrome("/usr/local/bin/chromedriver")
+#URL 얻기
+driver.get("http://blog.naver.com/lynn1128")
